b7cf5c26619f4181ac69d29cca9d81cbbfb2aa77
Boaz Kaufman <bk1nyc@gmail.com>
2024-07-26 15:58:12 -0400

    Fix typo in manuscript add form

--- manuscripts/add_form.py
+++ manuscripts/add_form.py
@@ -70,7 +70,7 @@ FORM_FLDS = [
                 ff.DISP_ON: FILE,
                 ff.FULL_WIDTH: True,
                 ff.HELPER: NOT_IMPLEMENTED,
-                ff.FILE_TYPE: [ff.DOCX, ff.HTML, ff.TXT, ff.MD]
+                ff.FILE_TYPES: [ff.DOCX, ff.HTML, ff.TXT, ff.MD]
             },
         ]
     },
